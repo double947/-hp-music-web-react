bfd34b9b4be17afb1e8379794a42dfe166da3880
double947 <1774387663@qq.com>
2021-07-29 18:46:51 +0800

    集成redux及thunk，配置redux-devTool，共享store

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { memo } from 'react'
+import { Provider } from 'react-redux'
 import { renderRoutes } from 'react-router-config'
-import { HashRouter, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
+import store from './store'
 import routes from './router'
 
 import { CoAppHeader, CoAppFooter } from 'components'
 
 export default memo(function App() {
   return (
-    <BrowserRouter>
-      <CoAppHeader />
-      {renderRoutes(routes)}
-      <CoAppFooter />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <CoAppHeader />
+          {renderRoutes(routes)}
+        <CoAppFooter />
+      </BrowserRouter>
+    </Provider>
   )
 })
