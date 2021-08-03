150ab2ade5da3524d7f1e7447dbd8033b7c43469
double947 <1774387663@qq.com>
2021-08-03 15:39:12 +0800

    使用 shallowEqual 来优化组件性能

--- src/views/discover/c-pages/recommend/index.js
+++ src/views/discover/c-pages/recommend/index.js
@@ -1,5 +1,5 @@
 import React, { memo, useEffect } from 'react'
-import { connect, useDispatch, useSelector } from 'react-redux'
+import { connect, shallowEqual, useDispatch, useSelector } from 'react-redux'
 
 import { getTopBannerAction } from './store/actionCreators'
 
@@ -8,7 +8,7 @@ function CoRecommend(props) {
   // 组件和redux关联：获取数据和进行dispatch操作
   const { topBanners } = useSelector(state => ({
     topBanners: state.recommend.topBanners
-  }))
+  }), shallowEqual)  // useSelector 的第二个参数传入 shallowEqual 会对当前组件所依赖的state做浅层比较，已达到性能优化的目的
   const dispatch = useDispatch()
 
   // 发送网络请求
