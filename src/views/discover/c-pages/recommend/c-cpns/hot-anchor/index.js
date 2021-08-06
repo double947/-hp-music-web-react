b402339dd2f6182ec8d94ef11d14c7cf4dc57aa2
double947 <1774387663@qq.com>
2021-08-06 18:56:17 +0800

    完成推荐模块中热门主播榜

--- src/views/discover/c-pages/recommend/c-cpns/hot-anchor/index.js
+++ src/views/discover/c-pages/recommend/c-cpns/hot-anchor/index.js
@@ -3,22 +3,48 @@ import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 
 import { getHotAnchorsAction } from '../../store/actionCreators'
 import { HOT_ANCHORS_LIMIT } from '@/common/constants'
-import { HotAnchorWrapper } from './style'
+import { HotAnchorWrapper, HotAnchorItemWrapper } from './style'
+import { getSizeImage } from '@/utils/format-utils'
+
+
+const AnchorItem = memo(function SingerItem(props) {
+  const { source } = props
+
+  return (
+    <HotAnchorItemWrapper className="flex mt2">
+      <img src={getSizeImage(source.avatarUrl, 50)} alt={source.nickName} />
+      <div className="flex flex-column pl2 info">
+        <a href="/" className="bold fs12 mt1 black name">{source.nickName}</a>
+        <div className="fs12 text-nowrap mt1 desc">{source.briefDesc || "主播"}</div>
+      </div>
+    </HotAnchorItemWrapper>
+  )
+})
 
 export default memo(function CoHotAnchor() {
   /* state */
+
+  /* redux */
   const { hotAnchors } = useSelector((state) => ({
     hotAnchors: state.getIn(['recommend', 'hotAnchors'])
   }), shallowEqual)
-  /* redux */
   const dispatch = useDispatch()
   /* hooks */
   useEffect(() => {
     dispatch(getHotAnchorsAction(HOT_ANCHORS_LIMIT))
   }, [dispatch])
   return (
-    <HotAnchorWrapper>
-      <h2>{hotAnchors.length}</h2>
+    <HotAnchorWrapper className="px2">
+      <div className="flex justify-between fitW pt2 pb1 header">
+        <div className="bold fs12 title">热门主播</div>
+      </div>
+      {
+        hotAnchors.map((item, index) => {
+          return (
+            <AnchorItem key={item.id} source={item} />
+          )
+        })
+      }
     </HotAnchorWrapper>
   )
 })
--- src/views/discover/c-pages/recommend/c-cpns/hot-anchor/style.js
+++ src/views/discover/c-pages/recommend/c-cpns/hot-anchor/style.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components'
 
 export const HotAnchorWrapper = styled.div`
+  height: 470px;
+  .header {
+    border-bottom: 1px solid #CCC;
+  }
+`
 
+export const HotAnchorItemWrapper = styled.div`
+  
 `
