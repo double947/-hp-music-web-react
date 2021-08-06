c9493f7d003fb39718ca01836b9551a6aee1c265
double947 <1774387663@qq.com>
2021-08-06 18:16:34 +0800

    完成入驻歌手榜

--- src/views/discover/c-pages/recommend/c-cpns/settle-singer/index.js
+++ src/views/discover/c-pages/recommend/c-cpns/settle-singer/index.js
@@ -3,7 +3,25 @@ import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 
 import { getSettledSingersAction } from '../../store/actionCreators'
 import { SETTLED_SINGERS_LIMIT, SETTLED_SINGERS_TYPE, SETTLED_SINGERS_AREA } from '@/common/constants'
-import { SettleSingerWrapper } from './style'
+import { SettleSingerWrapper, SingerItemWrapper } from './style'
+import { getSizeImage } from '@/utils/format-utils'
+
+
+
+const SingerItem = memo(function SingerItem(props) {
+  const { source } = props
+  console.log(source)
+
+  return (
+    <SingerItemWrapper className="flex mt2">
+      <img src={getSizeImage(source.img1v1Url, 62)} alt={source.name} />
+      <div className="flex flex-column pl2 info">
+        <div className="bold fs12 mt1 name">{source.name}</div>
+        <div className="fs12 text-nowrap mt1 desc">{source.briefDesc || "歌手"}</div>
+      </div>
+    </SingerItemWrapper>
+  )
+})
 
 
 export default memo(function CoSettleSinger() {
@@ -20,8 +38,20 @@ export default memo(function CoSettleSinger() {
   }, [dispatch])
   return (
     <SettleSingerWrapper className="flex flex-column items-center px2">
-    <div className="content"></div>
-    <div className="footer"></div>
+      <div className="flex justify-between fitW pt2 pb1 header">
+        <div className="bold fs12 title">入驻歌手</div>
+        <a href="/" className="fs12">查看全部 &gt;</a>
+      </div>
+      <div className="content">
+        {
+          settledSingers.map((item, index) => {
+            return (
+              <SingerItem key={item.id} source={item} />
+            )
+          })
+        }
+      </div>
+      <div className="flex justify-center items-center mt1 fs12 bold apply">申请成为网易音乐人</div>
     </SettleSingerWrapper>
   )
 })
--- src/views/discover/c-pages/recommend/c-cpns/settle-singer/style.js
+++ src/views/discover/c-pages/recommend/c-cpns/settle-singer/style.js
@@ -1,5 +1,43 @@
 import styled from 'styled-components'
 
 export const SettleSingerWrapper = styled.div`
-  height: 455px;
+  height: 470px;
+  .header {
+    border-bottom: 1px solid #CCC;
+  }
+  .apply {
+    cursor: pointer;
+    width: 210px;
+    height: 40px;
+    background-color: #F2F2F2;
+    border: 1px solid #ccc;
+    border-radius: 2px;
+    background-position: right -100px;
+    :hover {
+      background-color: #EDEDED;
+    }
+  }
+`
+
+export const SingerItemWrapper = styled.div`
+  cursor: pointer;
+  width: 210px;
+  height: 62px;
+  background-color: #fafafa;
+  overflow: hidden;
+  :hover {
+    background-color: #eee;
+  }
+  img {
+    width: 62px;
+    height: 62px;
+  }
+  .info {
+    flex: 1;
+    border: 1px solid #e9e9e9;
+    .desc {
+      width: 120px;
+      color: #666;
+    }
+  }
 `
