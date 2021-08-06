9ea487264bb063ef396e5c96a479cfd0e6ce4607
double947 <1774387663@qq.com>
2021-08-06 15:59:20 +0800

    实现引导用户登录组件UI

--- src/views/discover/c-pages/recommend/c-cpns/user-login/index.js
+++ src/views/discover/c-pages/recommend/c-cpns/user-login/index.js
@@ -4,8 +4,9 @@ import { UserLoginWrapper } from './style'
 
 export default memo(function CoUserLogin() {
   return (
-    <UserLoginWrapper>
-      <h2>CoUserLogin</h2>
+    <UserLoginWrapper className="flex flex-column items-center p2 sprite_02">
+      <div className="fs12 desc">登录网易云音乐，可以享受无限收藏的乐趣，并且无限同步到手机</div>
+      <div className="flex justify-center items-center white mt2 btn sprite_02">用户登录</div>
     </UserLoginWrapper>
   )
 })
--- src/views/discover/c-pages/recommend/c-cpns/user-login/style.js
+++ src/views/discover/c-pages/recommend/c-cpns/user-login/style.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components'
 
 export const UserLoginWrapper = styled.div`
-
+  height: 126px;
+  background-position: 0 0;
+  .desc {}
+  .btn {
+    width: 100px;
+    height: 31px;
+    cursor: pointer;
+    background-position: 0 -195px;
+  }
+  .btn:hover {
+    background-position: -110px -195px;
+  }
 `
